a24957ccdc13fe47eb4fd0cc1fac2c569f36a034
folathecoder <officialakinloyefolarin@gmail.com>
2021-08-24 18:28:34 +0100

    Fetched Data From Country Endpoint

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,5 @@
 //TODO: ==========>>>> THEME SWITCH
+const container = document.querySelector('.container__inner');
 
 const darkIcon = document.querySelector('.header-theme-dark-icon');
 const lightIcon = document.querySelector('.header-theme-light-icon');
@@ -25,3 +26,46 @@ themeBtn.addEventListener('click', function(e) {
         body.classList.add('dark-theme');
     }
 });
+
+//TODO: ==========>>>> FETCH ALL COUNTRY DATA
+const countryData = async function() {
+    try {
+
+        const response = await fetch('https://restcountries.eu/rest/v2/all');
+
+        //Throw a custom error
+        if (!response.ok) throw new Error('Could not fetch data from the API!')
+
+        const data = await response.json();
+        const res = data;
+
+        console.log(res);
+
+        //Render Fetch Data to the Homepage
+
+        const markup = res.map(country => {
+            return `
+            <div class="card">
+                <div class="card__image">
+                <img src="${country.flag}" alt="${country.name}" />
+                </div>
+                <div class="card__title"><h2>${country.name}</h2></div>
+                <div class="card__info">
+                <p>Population: <span>${country.population}</span></p>
+                <p>Region: <span>${country.region}</span></p>
+                <p>Capital: <span>${country.capital}</span></p>
+                </div>
+           </div>
+            `
+        }).join('');
+
+        container.innerHTML = "";
+        container.insertAdjacentHTML('beforeend', markup);
+
+
+    } catch (error) {
+        alert(error);
+    }
+}
+
+countryData();
